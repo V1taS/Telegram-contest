34e3587a58a9d321bece9e4b781a080372f6228f
Sosin Vitalii <375693@mail.ru>
2022-10-11 11:43:12 +0300

    Initial Commit

---
+++ Telegram contest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Telegram contest
+//
+//  Created by Vitalii Sosin on 11.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
